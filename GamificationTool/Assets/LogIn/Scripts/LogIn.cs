d938ccf07e41f4d39437e07a41dadb60a389ad5f
ga-abreuer <34494951+ga-abreuer@users.noreply.github.com>
2017-12-12 22:22:41 -0300

    Add New Project
    Adding a base proyect to start with

---
+++ GamificationTool/Assets/LogIn/Scripts/LogIn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogIn : MonoBehaviour {
+
+	public InputField userID;
+	public InputField password;
+
+	public Text errorText;
+	// Use this for initialization
+	void Start () {
+		errorText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void tryLogIn(){
+		if (CheckUser ()) {
+			SceneManager.LoadScene("MainMenu");
+		}
+	}
+
+	public bool CheckUser(){
+		Debug.Log (userID.text);
+		Debug.Log (password.text);
+		if (userID.text == "Alex" && password.text == "sofdos") {
+			return true;
+		} else {
+			errorText.text = "User or Password Incorrect.";
+			return false;
+		}
+	}
+
+
+}
