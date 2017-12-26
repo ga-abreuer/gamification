75c9b8efa2c0d59745b314ec9a8f9e61b362ce59
ga-abreuer <34494951+ga-abreuer@users.noreply.github.com>
2017-12-26 19:53:18 -0300

    CantRemeber...

---
+++ GamificationTool/Assets/Structure/Definitions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Definitions {
+
+	//This class is just for testing purposes, this information should be gathered from a database
+
+
+
+
+}
