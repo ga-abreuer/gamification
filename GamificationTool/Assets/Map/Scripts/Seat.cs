b17b948dc4d3810b1c127b5d134e87099830de14
ga-abreuer <34494951+ga-abreuer@users.noreply.github.com>
2017-12-28 15:37:26 -0300

    Map Update

---
+++ GamificationTool/Assets/Map/Scripts/Seat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seat : MonoBehaviour {
+
+	public string name;
+	public int docket;
+	public string team;
+	private ToolTip _ToolTip;
+	//Function that shows on the ToolTip the information of this seat.
+	public void ShowInfo(){
+		_ToolTip.gameObject.SetActive (true);
+		_ToolTip.name.text = name;
+		_ToolTip.docket.text = docket.ToString();
+		_ToolTip.team.text = team;
+		_ToolTip.target = this.gameObject;
+	}
+
+	void Awake(){
+		_ToolTip = GameObject.Find ("ToolTip").GetComponent<ToolTip>();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ GamificationTool/Assets/ToolTip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolTip : MonoBehaviour {
+
+	public Text name;
+	public Text docket;
+	public Text team;
+
+	public GameObject target;
+
+	void Start(){
+		gameObject.SetActive (false);
+	}
+
+
+	void Update () {
+		transform.position = Input.mousePosition;
+		if (target != null) {
+			if (Vector3.Distance (this.transform.position, target.transform.position) > 50.0f) {
+				gameObject.SetActive (false);
+				target = null;
+			}
+		} else {
+			target = null;
+		}
+	}
+}
